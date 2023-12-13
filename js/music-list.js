37751e91455fd7bbc56b77a977caf7d1cb5dfef6
SANCHIT MISHRA <103894484+Sanchit0228@users.noreply.github.com>
2023-12-13 19:57:16 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,83 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  
+ 
+  {
+    name: "Hey Krishna -  Krishna Aur Kans",
+    artist: "Sonu Nigam ",
+    img: " Song-1",
+    src: "Song-1"
+  },
+  {
+    name: "Rait Zara Si - Atrangi Re ",
+    artist: "Arijit Singh ||  Shashaa Tirupati",
+    img: "Song-2",
+    src: "song-2"
+  },
+  {
+    name: "Chaleya  - JAWAN",
+    artist: " Arijit S, Shilpa R",
+    img: "Song-3",
+    src: "Song-3"
+  },
+  {
+    name: " Saudebaazi - Mary kom ",
+    artist: "Arijit Singh",
+    img: "Song-4",
+    src: "Song-4"
+  },
+  {
+    name: "Main Parwaana -  Pippa ",
+    artist: " Arijit Singh - A. R. Rahman",
+    img: "Song-5",
+    src: "Song-5"
+  },
+  {
+    name: "One Bottle Down- T-SERIES",
+    artist: "Yo Yo Honey Singh ",
+    img: "Song-6",
+    src: "Song-6"
+  },
+ 
+  {
+    name: "Party With The Bhoothnath - Bhoothnath",
+    artist: "  Yo Yo Honey Singh",
+    img: "Song-8",
+    src: "song-8"
+  },
+  {
+    name: "MAKHNA  - T-SERIES",
+    artist: " Yo Yo Honey Singh - Neha Kakkar",
+    img: "Song-9",
+    src: "song-9"
+  },
+  {
+    name: "Pal -  Jalebi ",
+    artist: "  Arijit Singh - Shreya Ghoshal",
+    img: "Song-7",
+    src: "song-7"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
